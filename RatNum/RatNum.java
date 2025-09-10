b74f94e7d16f9802fb4277f5af530f08f2e5af79
Spirren <107792831+Spirren@users.noreply.github.com>
2025-09-10 18:46:32 +0200

    ce

--- RatNum/RatNum.java
+++ RatNum/RatNum.java
@@ -1,17 +1,29 @@
+/**
+ * This is Ratnum an implementation of ratinal numbers.
+ */
 public class RatNum {
     private int denominator;
     private int numerator;
 
+    /**
+     * constructor of RatNum with values 0/1
+     */
     public RatNum() {
         this.numerator = 0;
         this.denominator = 1;
     }
 
+    /**
+     * constructor of RatNum with values a/1
+     */
     public RatNum(int a) {
         this.numerator = a;
         this.denominator = 1;
     }
 
+    /**
+     * constructor of RatNum with values a/b
+     */
     public RatNum(int a, int b) {
         if (b == 0) {
             throw new NumberFormatException("cant devide with zero");
@@ -26,23 +38,45 @@ public class RatNum {
         this.denominator = b / gcd;
     }
 
+    /**
+     * constructor of RatNum with values mirrored from r
+     */
     public RatNum(RatNum r) {
         this.numerator = r.getNumerator();
         this.denominator = r.getDenominator();
     }
 
+    /**
+     * constructor of RatNum from the stirng passed
+     */
+    public RatNum(String s) {
+        this(parse(s));
+    }
+
+    /**
+     * returns denominator
+     */
     public int getDenominator() {
         return this.denominator;
     }
 
+    /**
+     * returns numerator
+     */
     public int getNumerator() {
         return this.numerator;
     }
 
+    /**
+     * returns string representation of the ratinal number
+     */
     public String toString() {
         return this.numerator + "/" + this.denominator;
     }
 
+    /**
+     * returns parsed string of the number
+     */
     public static RatNum parse(String s) {
         if (!s.contains("/")) {
             return new RatNum(Integer.parseInt(s));
@@ -54,10 +88,9 @@ public class RatNum {
             throw new NumberFormatException("Can only construct a Ratinal number form two integers");
     }
 
-    public RatNum(String s) {
-        this(parse(s));
-    }
-
+    /**
+     * returns if object r is equal to the number
+     */
     public boolean equals(Object r) {
         if (r == null || (r.getClass() != this.getClass())) {
             return false;
@@ -66,32 +99,53 @@ public class RatNum {
         return this.getDenominator() == ratnum.getDenominator() && this.getNumerator() == ratnum.getNumerator();
     }
 
+    /**
+     * returns boolean of the number less than r
+     */
     public boolean lessThan(RatNum r) {
         return (float) this.getNumerator() / this.getDenominator() < (float) r.getNumerator() / r.getDenominator();
     }
 
+    /**
+     * returns addition of r to the number
+     */
     public RatNum add(RatNum r) {
         return new RatNum(this.getNumerator() * r.getDenominator() + r.getNumerator() * this.getDenominator(),
                 this.getDenominator() * r.getDenominator());
     }
 
+    /**
+     * returns subtraction of r to the number
+     */
     public RatNum sub(RatNum r) {
         return new RatNum(this.getNumerator() * r.getDenominator() - r.getNumerator() * this.getDenominator(),
                 this.getDenominator() * r.getDenominator());
     }
 
+    /**
+     * returns multiplication of r with the number
+     */
     public RatNum mul(RatNum r) {
         return new RatNum(this.getNumerator() * r.getNumerator(), this.getDenominator() * r.getDenominator());
     }
 
+    /**
+     * returns division of the number with r
+     */
     public RatNum div(RatNum r) {
         return new RatNum(this.getNumerator() * r.getDenominator(), this.getDenominator() * r.getNumerator());
     }
 
+    /**
+     * returns string representation of the number
+     */
     public String toIntString() {
         return (Integer.toString(this.getNumerator() / this.getDenominator()));
     }
 
+    /**
+     * returns greatest comon divider of two integers, m and n
+     */
     public static int gcd(int m, int n) {
         m = Math.abs(m);
         n = Math.abs(n);
